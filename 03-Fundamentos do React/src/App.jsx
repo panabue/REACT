3751919ccfa81dba402f3c47faee6e018cb26508
Theo Nasser <theotnnasser@gmail.com>
2026-02-26 13:54:07 -0400

    feat: add TabButton component and integrate it into the App component

--- 03-Fundamentos do React/src/App.jsx
+++ 03-Fundamentos do React/src/App.jsx
@@ -1,6 +1,7 @@
 import {CORE_CONCEPTS} from './data.js'
 import Header from './components/Header/Header.jsx'
 import CoreConcepts from './components/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
@@ -16,6 +17,17 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[2]}/>
             <CoreConcepts {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>Jsx</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+          
         </section>
       </main>
     </div>
