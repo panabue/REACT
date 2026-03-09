d40ca68138c2189a876d69933e65a312b8be6f60
Theo Nasser <theotnnasser@gmail.com>
2026-03-09 13:41:32 -0400

    feat: game-over

--- 04-Aprofundando nos Fundamentos/02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 04-Aprofundando nos Fundamentos/02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,10 +1,4 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
+export default function GameBoard({ onSelectSquare, board }) {
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   // function handelSelectSquare(rowIndex, colIndex) {
@@ -16,24 +10,14 @@ export default function GameBoard({ onSelectSquare, turns }) {
 
   //   onSelectSquare();
   // }
-
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
                   {playerSymbol}
                 </button>
               </li>
---
+++ 04-Aprofundando nos Fundamentos/02-tic-tac-toe-starting-project/src/components/GameOver.jsx
@@ -0,0 +1,5 @@
+export default function GameOver() {
+  return <div id="game-over">
+    <h2>Game Over!</h2>
+  </div>
+}
