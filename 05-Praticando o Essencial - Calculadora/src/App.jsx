683c1a06f1ee71589f4d58dbf9587e626d7f83a5
Panabue <theotnnasser@gmail.com>
2026-03-11 23:55:19 -0400

    feat: starting app

--- 05-Praticando o Essencial - Calculadora/src/App.jsx
+++ 05-Praticando o Essencial - Calculadora/src/App.jsx
@@ -1,7 +1,52 @@
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <header id="header">
+        <img src="investment-calculator-logo.png" alt="Logo" />
+        <h1>Investment Calculator</h1>
+      </header>
+
+      <section id="user-input">
+        <div class="input-group">
+          <p>
+            <label>Initial Investment</label>
+            <input type="number" />
+          </p>
+          <p>
+            <label>Anual Investment</label>
+            <input type="number" />
+          </p>
+        </div>
+        <div class="input-group">
+          <p>
+            <label>Expected Return</label>
+            <input type="number" />
+          </p>
+          <p>
+            <label>Duration</label>
+            <input type="number" />
+          </p>
+        </div>
+      </section>
+
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Iterest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody class="center">
+          <tr>
+            <td>1</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
 
-export default App
+export default App;
