395669aba2745094cfa4ca7a858d0aec86831c47
Panabue <theotnnasser@gmail.com>
2026-03-16 22:50:20 -0400

    feat: functional calculator

--- 05-Praticando o Essencial - Calculadora/src/App.jsx
+++ 05-Praticando o Essencial - Calculadora/src/App.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import { calculateInvestmentResults, formatter } from "./util/investment";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: null,
+    annualInvestment: null,
+    expectedReturn: null,
+    duration: null,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+
+    setAnnualData(calculateInvestmentResults(userInput));
+  }
+
+  const annualData = calculateInvestmentResults(userInput);
+
   return (
     <>
       <header id="header">
@@ -7,24 +30,46 @@ function App() {
       </header>
 
       <section id="user-input">
-        <div class="input-group">
+        <div className="input-group">
           <p>
             <label>Initial Investment</label>
-            <input type="number" />
+            <input
+              type="number"
+              value={userInput.initialInvestment}
+              onChange={(event) =>
+                handleChange("initialInvestment", event.target.value)
+              }
+            />
           </p>
           <p>
             <label>Anual Investment</label>
-            <input type="number" />
+            <input
+              type="number"
+              value={userInput.annualInvestment}
+              onChange={(event) =>
+                handleChange("annualInvestment", event.target.value)
+              }
+            />
           </p>
         </div>
-        <div class="input-group">
+        <div className="input-group">
           <p>
             <label>Expected Return</label>
-            <input type="number" />
+            <input
+              type="number"
+              value={userInput.expectedReturn}
+              onChange={(event) =>
+                handleChange("expectedReturn", event.target.value)
+              }
+            />
           </p>
           <p>
             <label>Duration</label>
-            <input type="number" />
+            <input
+              type="number"
+              value={userInput.duration}
+              onChange={(event) => handleChange("duration", event.target.value)}
+            />
           </p>
         </div>
       </section>
@@ -39,10 +84,26 @@ function App() {
             <th>Invested Capital</th>
           </tr>
         </thead>
-        <tbody class="center">
-          <tr>
-            <td>1</td>
-          </tr>
+        <tbody className="center">
+          {annualData.map((yearData) => {
+            // 3. Calculando os dados ausentes para cada ano:
+            const totalInterest =
+              yearData.valueEndOfYear -
+              yearData.annualInvestment * yearData.year -
+              userInput.initialInvestment;
+
+            const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+            return (
+              <tr key={yearData.year}>
+                <td>{yearData.year}</td>
+                <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                <td>{formatter.format(yearData.interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalAmountInvested)}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </>
