8aa2f5c79aab91bd11cf59b2096aefee63998bda
Theo Nasser <theotnnasser@gmail.com>
2026-03-18 13:46:00 -0400

    feat: new components

--- 05-Praticando o Essencial - Calculadora/src/App.jsx
+++ 05-Praticando o Essencial - Calculadora/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { calculateInvestmentResults, formatter } from "./util/investment";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Result from "./components/Result";
 
 function App() {
   const [userInput, setUserInput] = useState({
@@ -16,96 +18,15 @@ function App() {
         [inputIdentifier]: +newValue,
       };
     });
-
-    setAnnualData(calculateInvestmentResults(userInput));
   }
-
-  const annualData = calculateInvestmentResults(userInput);
 
   return (
     <>
-      <header id="header">
-        <img src="investment-calculator-logo.png" alt="Logo" />
-        <h1>Investment Calculator</h1>
-      </header>
+      <Header />
 
-      <section id="user-input">
-        <div className="input-group">
-          <p>
-            <label>Initial Investment</label>
-            <input
-              type="number"
-              value={userInput.initialInvestment}
-              onChange={(event) =>
-                handleChange("initialInvestment", event.target.value)
-              }
-            />
-          </p>
-          <p>
-            <label>Anual Investment</label>
-            <input
-              type="number"
-              value={userInput.annualInvestment}
-              onChange={(event) =>
-                handleChange("annualInvestment", event.target.value)
-              }
-            />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label>Expected Return</label>
-            <input
-              type="number"
-              value={userInput.expectedReturn}
-              onChange={(event) =>
-                handleChange("expectedReturn", event.target.value)
-              }
-            />
-          </p>
-          <p>
-            <label>Duration</label>
-            <input
-              type="number"
-              value={userInput.duration}
-              onChange={(event) => handleChange("duration", event.target.value)}
-            />
-          </p>
-        </div>
-      </section>
+      <UserInput userInput={userInput} handleChange={handleChange} />
 
-      <table id="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Investment Value</th>
-            <th>Iterest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody className="center">
-          {annualData.map((yearData) => {
-            // 3. Calculando os dados ausentes para cada ano:
-            const totalInterest =
-              yearData.valueEndOfYear -
-              yearData.annualInvestment * yearData.year -
-              userInput.initialInvestment;
-
-            const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
-
-            return (
-              <tr key={yearData.year}>
-                <td>{yearData.year}</td>
-                <td>{formatter.format(yearData.valueEndOfYear)}</td>
-                <td>{formatter.format(yearData.interest)}</td>
-                <td>{formatter.format(totalInterest)}</td>
-                <td>{formatter.format(totalAmountInvested)}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <Result userInput={userInput} />
     </>
   );
 }
